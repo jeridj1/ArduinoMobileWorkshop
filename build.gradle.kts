fc16adea5e71a7ba6d6ccdaea196858d045a04e6
jeridj1 <jeriddrako22@gmail.com>
2026-08-17 15:40:34 -0700

    Update root build.gradle.kts with clean configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,23 +1,18 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+
 plugins {
-    id("com.android.application") version "8.3.1" apply false
-    id("com.android.library") version "8.3.1" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    id("com.android.application") version "8.3.0" apply false
+    id("com.android.library") version "8.3.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.20" apply false
 }
 
-// Common dependencies
-val kotlinVersion = "1.9.22"
-val androidGradlePluginVersion = "8.3.1"
-
-task clean(type: Delete) {
-    delete rootProject.buildDir
-}
-
-// Add repositories for all modules
 allprojects {
     repositories {
         google()
         mavenCentral()
-        maven { url = "https://jitpack.io" }
     }
+}
+
+task clean(type: Delete) {
+    delete rootProject.buildDir
 }
