191af808601e6205e4fb81260b1921a7402be195
jeridj1 <jeriddrako22@gmail.com>
2026-08-17 14:39:31 -0700

    Update settings.gradle.kts to include all modules

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,9 +1,7 @@
-// Arduino Mobile Workshop - Project Settings
 rootProject.name = "ArduinoMobileWorkshop"
 
-// Include all modules
 include(":app")
 include(":toolchain")
 include(":workspace")
 include(":usb")
-include(":hardware:rp2040-bridge")
+include(":rp2040")
