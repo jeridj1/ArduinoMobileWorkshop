c32b7a0bcaaabf5e73c4180eb15d1a9c73c0695d
jeridj1 <jeriddrako22@gmail.com>
2026-08-17 15:40:46 -0700

    Update app/build.gradle.kts with clean configuration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,91 +6,51 @@ plugins {
 android {
     namespace = "com.arduinomobileworkshop.app"
     compileSdk = 34
-    
+
     defaultConfig {
         applicationId = "com.arduinomobileworkshop.app"
         minSdk = 24
         targetSdk = 34
         versionCode = 1
         versionName = "1.0.0"
-        
+
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
-    
-    signingConfigs {
-        release {
-        }
-    }
-    
+
     buildTypes {
         release {
             minifyEnabled(false)
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
-        debug {
-            applicationIdSuffix = ".debug"
-            debuggable = true
-        }
     }
-    
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
-    
+
     kotlinOptions {
         jvmTarget = "1.8"
     }
-    
+
     buildFeatures {
         viewBinding = true
     }
-    
-    packaging {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-        }
-    }
-}
-
-repositories {
-    google()
-    mavenCentral()
-    maven { url = "https://jitpack.io" }
 }
 
 dependencies {
-    // AndroidX Core
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.gridlayout:gridlayout:1.0.0")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
     
     // USB Serial
     implementation("com.hoho.android:usb-serial:1.2.0")
     
     // Coroutines
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
-    
-    // Lifecycle
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
-    implementation("androidx.lifecycle:lifecycle-common-java8:2.7.0")
-    
-    // Activity & Fragment
-    implementation("androidx.activity:activity-ktx:1.8.2")
-    implementation("androidx.fragment:fragment-ktx:1.7.0")
-    
-    // Preferences
-    implementation("androidx.preference:preference-ktx:1.2.1")
-    
-    // RecyclerView
-    implementation("androidx.recyclerview:recyclerview:1.3.2")
-    
-    // File picker library
-    implementation("com.github.angads25:filepicker:1.0.6")
     
     // Modules
     implementation(project(":toolchain"))
@@ -98,11 +58,5 @@ dependencies {
     implementation(project(":usb"))
     implementation(project(":rp2040"))
     
-    // Testing
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation("androidx.test:runner:1.5.2")
-    androidTestImplementation("androidx.test:rules:1.5.0")
-    androidTestImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
 }
