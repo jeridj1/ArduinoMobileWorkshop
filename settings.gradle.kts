32504f7f418ccebddd7ca5610fea5714ce108af3
jeridj1 <jeriddrako22@gmail.com>
2026-08-17 14:04:46 -0700

    Add root build files

---
+++ settings.gradle.kts
@@ -0,0 +1,9 @@
+// Arduino Mobile Workshop - Project Settings
+rootProject.name = "ArduinoMobileWorkshop"
+
+// Include all modules
+include(":app")
+include(":toolchain")
+include(":workspace")
+include(":usb")
+include(":hardware:rp2040-bridge")
