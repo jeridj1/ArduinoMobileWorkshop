9cc7d1282b7ba5e06d08405ff629f283eec68272
jeridj1 <jeriddrako22@gmail.com>
2026-08-17 15:08:13 -0700

    Update app build.gradle.kts with navigation dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,11 +56,16 @@ android {
 }
 
 dependencies {
-    // AndroidX
+    // AndroidX Core
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    
+    // Navigation Components
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
+    implementation("androidx.navigation:navigation-dynamic-features-fragment:2.7.7")
     
     // USB Serial
     implementation("com.hoho.android:usb-serial:1.2.0")
@@ -74,16 +79,14 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
     implementation("androidx.lifecycle:lifecycle-common-java8:2.7.0")
     
-    // Activity
+    // Activity & Fragment
     implementation("androidx.activity:activity-ktx:1.8.2")
-    
-    // Fragment
     implementation("androidx.fragment:fragment-ktx:1.7.0")
     
     // Preferences
     implementation("androidx.preference:preference-ktx:1.2.1")
     
-    // File picker
+    // File picker library
     implementation("com.github.angads25:filepicker:1.0.6")
     
     // Modules
