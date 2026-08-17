dd1b9eed2f00ac3e6f39381a9619e1c3f46d7dd0
jeridj1 <jeriddrako22@gmail.com>
2026-08-17 14:04:50 -0700

    Add root build files

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+// Arduino Mobile Workshop - Root Build Configuration
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.2.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0")
+    }
+}
+
+plugins {
+    id("com.android.application") version "8.2.0" apply false
+    id("com.android.library") version "8.2.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+}
+
+tasks.named("clean") {
+    delete(file("build"))
+}
