24da3858522b6a21c97916a74d702fcf05c9c55a
jeridj1 <jeriddrako22@gmail.com>
2026-08-17 23:20:46 -0700

    Fix Gradle plugin and dependency repository resolution

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,20 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://jitpack.io") }
+    }
+}
+
 rootProject.name = "ArduinoMobileWorkshop"
 
 include(":app")
