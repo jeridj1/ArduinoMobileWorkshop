0e91ff48b54c7df5a251f176d6a0be63fd771123
jeridj1 <jeriddrako22@gmail.com>
2026-08-17 21:29:54 -0700

    Fix Windows Gradle wrapper path and missing-jar bootstrap

--- gradlew.bat
+++ gradlew.bat
@@ -1,13 +1,18 @@
-@rem
 @echo off
-@if "%DEBUG%"=="" @echo on
-@rem Set local directory
-set LOCAL_DIR=%~dp0
-@rem Find java.exe
-set DEFAULT_JAVACMD=java
-set JAVA_EXE=%JAVA_HOME%/bin/java.exe
-if exist "%JAVA_EXE%" set DEFAULT_JAVACMD=%JAVA_EXE%
-@rem Setup the command line
-set CLASSPATH=%LOCAL_DIR%gradlewrappergradle-wrapper.jar
-@rem Execute Gradle
-%DEFAULT_JAVACMD% -cp "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
+setlocal
+
+set "APP_HOME=%~dp0"
+set "WRAPPER_JAR=%APP_HOME%gradle\wrapper\gradle-wrapper.jar"
+
+if not exist "%WRAPPER_JAR%" (
+  if not exist "%APP_HOME%gradle\wrapper" mkdir "%APP_HOME%gradle\wrapper"
+  powershell -NoProfile -ExecutionPolicy Bypass -Command "Invoke-WebRequest -UseBasicParsing -Uri 'https://raw.githubusercontent.com/gradle/gradle/v8.6.0/gradle/wrapper/gradle-wrapper.jar' -OutFile '%WRAPPER_JAR%'"
+  if errorlevel 1 exit /b 1
+)
+
+set "CLASSPATH=%WRAPPER_JAR%"
+if defined JAVA_HOME if exist "%JAVA_HOME%\bin\java.exe" set "JAVA_EXE=%JAVA_HOME%\bin\java.exe"
+if not defined JAVA_EXE set "JAVA_EXE=java"
+
+"%JAVA_EXE%" -cp "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
+endlocal
