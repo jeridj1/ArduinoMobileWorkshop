bf5743bd29924f9feb3766d90e4f47ab2a95dcd6
jeridj1 <jeriddrako22@gmail.com>
2026-08-23 00:09:18 -0700

    Handle USB attach, detach, and permission broadcasts

--- app/src/main/java/com/arduinomobileworkshop/app/usb/UsbDeviceReceiver.kt
+++ app/src/main/java/com/arduinomobileworkshop/app/usb/UsbDeviceReceiver.kt
@@ -6,30 +6,28 @@ import android.content.Intent
 import android.hardware.usb.UsbDevice
 import android.hardware.usb.UsbManager
 import android.util.Log
+import com.arduinomobileworkshop.app.ArduinoMobileWorkshopApp
+import com.arduinomobileworkshop.usb.UsbSerialManager
 
 class UsbDeviceReceiver : BroadcastReceiver() {
     companion object {
         private const val TAG = "AMW_USB_Receiver"
     }
-    
+
     override fun onReceive(context: Context, intent: Intent) {
+        val device = intent.getParcelableExtra<UsbDevice>(UsbManager.EXTRA_DEVICE)
         when (intent.action) {
-            UsbManager.ACTION_USB_DEVICE_ATTACHED -> {
-                val device = intent.getParcelableExtra<UsbDevice>(UsbManager.EXTRA_DEVICE)
-                device?.let { onDeviceAttached(context, it) }
+            UsbManager.ACTION_USB_DEVICE_ATTACHED -> device?.let {
+                ArduinoMobileWorkshopApp.instance.usbManager.onDeviceAttached(it)
             }
-            UsbManager.ACTION_USB_DEVICE_DETACHED -> {
-                val device = intent.getParcelableExtra<UsbDevice>(UsbManager.EXTRA_DEVICE)
-                device?.let { onDeviceDetached(context, it) }
+            UsbManager.ACTION_USB_DEVICE_DETACHED -> device?.let {
+                ArduinoMobileWorkshopApp.instance.usbManager.onDeviceDetached(it)
+            }
+            UsbSerialManager.ACTION_USB_PERMISSION -> if (device != null && intent.getBooleanExtra(UsbManager.EXTRA_PERMISSION_GRANTED, false)) {
+                ArduinoMobileWorkshopApp.instance.usbManager.onPermissionGranted(device)
+            } else {
+                Log.w(TAG, "USB permission denied")
             }
         }
-    }
-    
-    private fun onDeviceAttached(context: Context, device: UsbDevice) {
-        Log.d(TAG, "Device attached: ${device.deviceName}")
-    }
-    
-    private fun onDeviceDetached(context: Context, device: UsbDevice) {
-        Log.d(TAG, "Device detached: ${device.deviceName}")
     }
 }
